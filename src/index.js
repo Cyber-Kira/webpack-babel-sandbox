6ef6b0e4781867217a25938980b38d5971f6e7d8
Cyber-Kira <kirillkoloskovm@gmail.com>
2021-04-03 16:04:58 +0300

    add webpack loader, add img

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import Log from "./log";
 import Calc from "./calc";
 
+import img from "./react.png";
+
 const calc = new Calc();
 const log = new Log();
 
 log.log(calc.add(1, 2, 3));
+
+const elem = document.createElement("img");
+elem.src = img;
+document.body.appendChild(elem);
