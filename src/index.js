6ff53de5f2b036508ff581689789d022eb3fcaff
Cyber-Kira <kirillkoloskovm@gmail.com>
2021-04-03 16:24:27 +0300

    add config for JavaScript files using babel-loader for webpack

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
-import Log from "./log";
-import Calc from "./calc";
+import React from "react";
+import ReactDOM from "react-dom";
 
-import img from "./react.png";
+const App = () => <p>Hello!</p>;
 
-const calc = new Calc();
-const log = new Log();
-
-log.log(calc.add(1, 2, 3));
-
-const elem = document.createElement("img");
-elem.src = img;
-document.body.appendChild(elem);
+ReactDOM.render(<App />, document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,47 @@ module.exports = {
 
   module: {
     rules: [
+      //
+      // Loading JavaScript
+      //
       {
-        test: /\.png$/,
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+          },
+        ],
+      },
+
+      //
+      // Loading Images
+      //
+      {
+        test: /\.(png|jpg|jpeg|gif|ico)$/,
         use: [
           {
             loader: "file-loader",
+            options: {
+              outputPath: "images",
+              name: "[name]-[sha1:hash:7].[ext]",
+            },
+          },
+        ],
+      },
+
+      //
+      // Loading Fonts
+      //
+      {
+        test: /\.(ttf|otf|eot|woff|woff2)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: "fonts",
+              name: "[name].[ext]",
+            },
           },
         ],
       },
